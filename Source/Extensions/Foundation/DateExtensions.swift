a5fc26df3c6722c4a9a0019f57408738595b7c85
Sebastián Varela <sebastian.varela@masmovil.com>
2019-04-10 16:37:34 +0200

    Add Date Extensions

---
+++ Source/Extensions/Foundation/DateExtensions.swift
@@ -0,0 +1,82 @@
+import Foundation
+
+public extension Date {
+    init(millisecondsSince1970: Double) {
+        self = Date(timeIntervalSince1970: millisecondsSince1970 / 1_000)
+    }
+
+    init?(formattedSpanishFullDate: String, timeZone: TimeZone? = nil) {
+        let formatter = Date.spanishFormatter
+        formatter.dateFormat = "dd/MM/yyyy HH:mm"
+        if let timeZone = timeZone {
+            formatter.timeZone = timeZone
+        }
+        guard let date = formatter.date(from: formattedSpanishFullDate) else {
+            return nil
+        }
+        self = date
+    }
+
+    var millisecondsSince1970: Double {
+        return timeIntervalSince1970 * 1_000.0
+    }
+
+    var day: String {
+        return formatWith(dateFormat: "d")
+    }
+
+    var monthSpanishNameAndYear: String {
+        return formatWith(dateFormat: "MMMM yyyy")
+    }
+
+    var monthSpanishName: String {
+        return formatWith(dateFormat: "MMMM")
+    }
+
+    var shortMonthSpanishName: String {
+        return formatWith(dateFormat: "MMM")
+    }
+
+    var year: String {
+        return formatWith(dateFormat: "yyyy")
+    }
+
+    var shortYear: String {
+        return formatWith(dateFormat: "yy")
+    }
+
+    func formattedSpanishDate(timeZone: TimeZone? = nil) -> String {
+        return formatWith(dateFormat: "dd 'de' MMMM", timeZone: timeZone)
+    }
+
+    func formattedSpanishFullDate(timeZone: TimeZone? = nil) -> String {
+        return formatWith(dateFormat: "dd/MM/yyyy HH:mm", timeZone: timeZone)
+    }
+
+    var isInTheFirstElevenDaysOfTheMonth: Bool {
+        let day = Calendar.current.component(.day, from: self)
+        return 1...11 ~= day
+    }
+
+    var isToday: Bool {
+        let calendar = Calendar(identifier: Calendar.Identifier.gregorian)
+        return calendar.isDateInToday(self)
+    }
+
+    // MARK: - Private methods
+
+    private func formatWith(dateFormat: String, timeZone: TimeZone? = nil) -> String {
+        let formatter = Date.spanishFormatter
+        formatter.dateFormat = dateFormat
+        if let timeZone = timeZone {
+            formatter.timeZone = timeZone
+        }
+        return formatter.string(from: self)
+    }
+
+    private static var spanishFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "es_ES")
+        return formatter
+    }
+}
---
+++ Tests/Extensions/Foundation/DateExtensionsTests.swift
@@ -0,0 +1,55 @@
+import XCTest
+import Foundation
+import MagicPills
+
+class DateExtensionsTests: XCTestCase {
+    func test_init_with_milliseconds() {
+        let milliseconds: Double = 12_408_124_000
+        let date = Date(millisecondsSince1970: milliseconds)
+
+        XCTAssertEqual(date.millisecondsSince1970, milliseconds)
+    }
+
+    func test_init_with_formatted_spanishDate() {
+        let spanishFullDate = "20/02/2018 12:33"
+        let date = Date(formattedSpanishFullDate: spanishFullDate)
+
+        XCTAssertEqual(date?.formattedSpanishFullDate(), spanishFullDate)
+        XCTAssertNil(Date(formattedSpanishFullDate: "👋"))
+    }
+
+    func test_date_parts() {
+        let timeZone = TimeZone(secondsFromGMT: 3_600)
+        let date = Date(formattedSpanishFullDate: "20/02/2018 12:33", timeZone: timeZone)
+
+        XCTAssertEqual(date?.day, "20")
+        XCTAssertEqual(date?.monthSpanishName, "febrero")
+        XCTAssertEqual(date?.monthSpanishNameAndYear, "febrero 2018")
+        XCTAssertEqual(date?.shortMonthSpanishName, "feb")
+        XCTAssertEqual(date?.year, "2018")
+        XCTAssertEqual(date?.shortYear, "18")
+        XCTAssertEqual(date?.day, "20")
+    }
+
+    func test_spanish_format_date() {
+        let date = Date(millisecondsSince1970: 1_214_124_999_000)
+
+        XCTAssertEqual(date.formattedSpanishFullDate(timeZone: TimeZone(secondsFromGMT: 0)), "22/06/2008 08:56")
+        XCTAssertEqual(date.formattedSpanishFullDate(timeZone: TimeZone(secondsFromGMT: 3_600)), "22/06/2008 09:56")
+        XCTAssertEqual(date.formattedSpanishDate(timeZone: TimeZone(secondsFromGMT: 0)), "22 de junio")
+        XCTAssertEqual(date.formattedSpanishDate(timeZone: TimeZone(secondsFromGMT: 3_600)), "22 de junio")
+    }
+
+    func test_is_today() {
+        XCTAssertTrue(Date().isToday)
+        XCTAssertFalse(Date().addingTimeInterval(-86_400).isToday)
+    }
+
+    func test_is_in_the_first_eleven_days_of_the_month() {
+        stride(from: 1, to: 31, by: 1).forEach { day in
+            let date = Date(formattedSpanishFullDate: "\(day)/01/2018 11:11")
+
+            XCTAssertEqual(date?.isInTheFirstElevenDaysOfTheMonth, day <= 11)
+        }
+    }
+}
