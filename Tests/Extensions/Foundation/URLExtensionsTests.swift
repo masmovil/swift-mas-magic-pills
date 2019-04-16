800ed9390436120803721a3c106fe1acae568524
Sebastián Varela <sebastian.varela@masmovil.com>
2019-04-16 17:59:51 +0200

    Add URL Extensions

---
+++ Tests/Extensions/Foundation/URLExtensionsTests.swift
@@ -0,0 +1,13 @@
+import XCTest
+import Foundation
+import MagicPills
+
+class URLExtensionsTests: XCTestCase {
+    func test_comparable() {
+        let url1 = URL(string: "http://aaa.com")!
+        let url2 = URL(string: "http://bbb.com")!
+
+        XCTAssertTrue(url1 < url2)
+        XCTAssertFalse(url1 > url2)
+    }
+}
