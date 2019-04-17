f59294d2ff222ebc63c89341bf8498a6524b3eeb
Sebastián Varela <sebastian.varela@masmovil.com>
2019-04-17 14:28:30 +0200

    Add Documentation

--- Source/Extensions/Foundation/SequenceExtensions.swift
+++ Source/Extensions/Foundation/SequenceExtensions.swift
@@ -1,6 +1,10 @@
 import Foundation
 
 public extension Sequence {
+    /// Group the sequence and return a dictionary using the given key in the closure.
+    ///
+    /// - Parameter key: Closure that generate the key of every element. The return value must be `Hashable`.
+    /// - Returns: A dictionary with all elements grouped by the given key.
     func group<U: Hashable>(by key: (Iterator.Element) -> U) -> [U: [Iterator.Element]] {
         var categories: [U: [Iterator.Element]] = [:]
         for element in self {
