8310cf440305ce672fc226367675a897c61a31a7
Sebastián Varela <sebastian.varela@masmovil.com>
2019-05-06 15:47:05 +0200

    Expose DefinesPrimaryKey

--- Source/Foundation/Protocols/DefinesPrimaryKey.swift
+++ Source/Foundation/Protocols/DefinesPrimaryKey.swift
@@ -1,11 +1,11 @@
 import Foundation
 
-protocol DefinesPrimaryKey: Equatable {
+public protocol DefinesPrimaryKey: Equatable {
     associatedtype PrimaryKey
 
     var primaryKey: PrimaryKey { get }
 }
 
-func ==<T: DefinesPrimaryKey>(lhs: T, rhs: T) -> Bool where T.PrimaryKey: Equatable {
+public func ==<T: DefinesPrimaryKey>(lhs: T, rhs: T) -> Bool where T.PrimaryKey: Equatable {
     return lhs.primaryKey == rhs.primaryKey
 }
