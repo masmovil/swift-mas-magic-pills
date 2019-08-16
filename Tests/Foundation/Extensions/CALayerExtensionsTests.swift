5c11d88faca24206b6623ed68a760cfe95a66aef
Edi López <edilberto.lopez@bq.com>
2019-08-16 09:10:46 +0200

    Removed extension from watchos

--- Tests/Foundation/Extensions/CALayerExtensionsTests.swift
+++ Tests/Foundation/Extensions/CALayerExtensionsTests.swift
@@ -9,22 +9,22 @@ class CALayerExtensionsTests: XCTestCase {
         let frame = CGRect(x: 0, y: 0, width: 100, height: 100)
         let view = UIView(frame: frame)
         let dummySubLayer = CALayer()
-        
+
         dummySubLayer.frame = view.layer.bounds
         view.layer.insertSublayer(dummySubLayer, at: 0)
-        
+
         view.layer.roundSublayers(20)
         XCTAssertEqual(view.layer.sublayers?.first!.cornerRadius, 20)
     }
-    
+
     func test_unround_sublayer() {
         let frame = CGRect(x: 0, y: 0, width: 100, height: 100)
         let view = UIView(frame: frame)
         let dummySubLayer = CALayer()
-        
+
         dummySubLayer.frame = view.layer.bounds
         view.layer.insertSublayer(dummySubLayer, at: 0)
-        
+
         view.layer.roundSublayers(20)
         XCTAssertEqual(view.layer.sublayers?.first!.cornerRadius, 20)
         view.layer.unroundSublayers()
