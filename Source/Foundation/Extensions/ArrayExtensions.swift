8b43a1c6456fec8e797d641161cc5ef1f56b5fdd
Sebastián Varela <sebastian.varela@masmovil.com>
2020-08-12 14:59:09 +0200

    Improve collection extensions

--- Source/Foundation/Extensions/ArrayExtensions.swift
+++ Source/Foundation/Extensions/ArrayExtensions.swift
@@ -5,11 +5,6 @@ public extension Array where Element: Hashable {
     var unique: [Element] {
         return Array(Set(self))
     }
-
-    /// Return false or true when array is empty or not
-    var isNotEmpty: Bool {
-        return !self.isEmpty
-    }
 }
 
 public extension Array where Element: Equatable {
@@ -23,6 +18,46 @@ public extension Array where Element: Equatable {
         return false
     }
 
+    /// Update existing elements or insert if not exists
+    mutating func upsert(_ newElements: [Element]) {
+        newElements.forEach { element in
+            upsert(element)
+        }
+    }
+
+    /// Update existing element or insert if not exists
+    mutating func upsert(_ newElement: Element) {
+        if let element = filter({ $0 == newElement }).first,
+            let index = firstIndex(of: element) {
+            remove(at: index)
+            insert(newElement, at: index)
+        } else {
+            append(newElement)
+        }
+    }
+
+    /// Update existing elements or insert if not exists
+    func upserting(_ newElements: [Element]) -> [Element] {
+        var result = self
+        newElements.forEach { element in
+            result = result.upserting(element)
+        }
+        return result
+    }
+
+    /// Update existing element or insert if not exists
+    func upserting(_ newElement: Element) -> [Element] {
+        var result = self
+        if let element = result.first(where: { $0 == newElement }),
+            let index = result.firstIndex(of: element) {
+            result.remove(at: index)
+            result.insert(newElement, at: index)
+        } else {
+            result.append(newElement)
+        }
+        return result
+    }
+
     /// Append the given element if not exists in array
     @discardableResult
     mutating func appendIfNotExists(_ element: Element) -> Bool {
@@ -31,5 +66,33 @@ public extension Array where Element: Equatable {
             return true
         }
         return false
+    }
+
+    /// Append the given element if not exists in array
+    @discardableResult
+    mutating func appendIfNotExists(_ elements: [Element]) -> Bool {
+        var result = false
+        elements.forEach { element in
+            result = appendIfNotExists(element) || result
+        }
+        return result
+    }
+
+    /// Return a copy of the array by appending the given element if not exists in array
+    func appendingIfNotExists(_ element: Element) -> [Element] {
+        var result = self
+        if !result.contains(element) {
+            result.append(element)
+        }
+        return result
+    }
+
+    /// Return a copy of the array by appending the given elements if not exists in array
+    func appendingIfNotExists(_ elements: [Element]) -> [Element] {
+        var result = self
+        elements.forEach { element in
+            result = result.appendingIfNotExists(element)
+        }
+        return result
     }
 }
---
+++ Source/Foundation/Types/KeyedValue.swift
@@ -0,0 +1,43 @@
+import Foundation
+
+public protocol KeyedValueType {
+    associatedtype Value
+
+    var id: String { get }
+    var value: Value { get }
+}
+
+public struct KeyedValue<Value: Codable>: KeyedValueType, Equatable, Codable {
+    public let id: String
+    public let value: Value
+
+    public init(id: String, value: Value) {
+        self.id = id
+        self.value = value
+    }
+
+    // MARK: - Codable
+    internal enum CodingKeys: String, CodingKey {
+        case id
+        case value
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        self.id = try container.decode(String.self, forKey: .id)
+        self.value = try container.decode(Value.self, forKey: .value)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(value, forKey: .value)
+    }
+
+    // MARK: - Equatable
+
+    public static func == (lhs: KeyedValue<Value>, rhs: KeyedValue<Value>) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
