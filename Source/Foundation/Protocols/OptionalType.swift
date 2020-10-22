1ff0d75e92ebb938c43aa1e6625a5b96ccf8e60e
Sebastián Varela <sebastian.varela@masmovil.com>
2020-10-22 23:27:10 +0200

    Add global unwrapTuple func to easily remove tuples with optionals value

--- Source/Foundation/Protocols/OptionalType.swift
+++ Source/Foundation/Protocols/OptionalType.swift
@@ -17,7 +17,47 @@ public extension Sequence where Iterator.Element: OptionalType {
     /// Remove all the nullable values inside a nullable values array.
     ///
     /// - Returns: A Collection without null values.
+    var filterNil: [Iterator.Element.Wrapped] {
+        return compactMap { $0.wrapped }
+    }
+
+    @available(*, deprecated, renamed: "filterNil")
     var filterNils: [Iterator.Element.Wrapped] {
-        return self.compactMap { $0.wrapped }
+        return filterNil
+    }
+}
+
+/// Return nil if any of his components is nil
+public func unwrapTuple<A, B>(first: A?, second: B?) -> (A, B)? {
+    return first.flatMap { firstUnwrapped -> (A, B)? in
+        second.flatMap { secondUnwrapped -> (A, B)? in
+            return (firstUnwrapped, secondUnwrapped)
+        }
+    }
+}
+
+/// Return nil if any of his components is nil
+// swiftlint:disable large_tuple
+public func unwrapTuple<A, B, C>(first: A?, second: B?, third: C?) -> (A, B, C)? {
+    return first.flatMap { firstUnwraped -> (A, B, C)? in
+        second.flatMap { secondUnwrapped -> (A, B, C)? in
+            third.flatMap { thirdUnwrapped -> (A, B, C)? in
+                return (firstUnwraped, secondUnwrapped, thirdUnwrapped)
+            }
+        }
+    }
+}
+
+/// Return nil if any of his components is nil
+// swiftlint:disable large_tuple
+public func unwrapTuple<A, B, C, D>(first: A?, second: B?, third: C?, fourth: D?) -> (A, B, C, D)? {
+    return first.flatMap { firstUnwraped -> (A, B, C, D)? in
+        second.flatMap { secondUnwrapped -> (A, B, C, D)? in
+            third.flatMap { thirdUnwrapped -> (A, B, C, D)? in
+                fourth.flatMap { fourthUnwrapped -> (A, B, C, D)? in
+                    return (firstUnwraped, secondUnwrapped, thirdUnwrapped, fourthUnwrapped)
+                }
+            }
+        }
     }
 }
