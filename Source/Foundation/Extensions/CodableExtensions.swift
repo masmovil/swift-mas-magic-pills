d950622e9f94c793efd3df1ccfcae26c76b5882b
Sebastián Varela <sebastian.varela@masmovil.com>
2021-08-11 17:20:58 +0200

    Fix tests

--- Source/Foundation/Extensions/CodableExtensions.swift
+++ Source/Foundation/Extensions/CodableExtensions.swift
@@ -2,15 +2,29 @@ import Foundation
 
 public extension Encodable {
     var jsonString: String? {
-        guard let jsonData = try? JSONEncoder().encode(self) else { return nil }
-        guard let json = String(data: jsonData, encoding: String.Encoding.utf8) else { return nil }
+        guard let data = try? JSONEncoder().encode(self) else { return nil }
+        guard let json = String(data: data, encoding: String.Encoding.utf8) else { return nil }
         return json
+    }
+
+    /// Return encodable object like a dictionary
+    func asDictionary() throws -> [String: Any] {
+        let data = try JSONEncoder().encode(self)
+        guard let dictionary = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any] else {
+            throw MagicError.cantConvertToDictionary
+        }
+        return dictionary
     }
 }
 
 public extension Decodable {
     static func decode(jsonString: String) -> Self? {
-        let data = jsonString.dataUTF8
+        decode(jsonString: (jsonString as String?))
+    }
+
+    static func decode(jsonString: String?) -> Self? {
+        guard let json = jsonString else { return nil }
+        let data = json.dataUTF8
         return try? JSONDecoder().decode(self, from: data)
     }
 }
---
+++ Tests/Helpers/CodableTestObject.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+struct CodableTestObject: Codable, Equatable {
+    let name: String
+    let age: Int
+    let weight: Float
+}
