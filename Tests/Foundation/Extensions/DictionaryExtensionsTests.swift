839a4fdd1a7c49ea63f33e1e7d48fe9b033408b7
Sebastián Varela <sebastian.varela@masmovil.com>
2021-08-11 14:02:13 +0200

    Cleanup

--- Tests/Foundation/Extensions/DictionaryExtensionsTests.swift
+++ Tests/Foundation/Extensions/DictionaryExtensionsTests.swift
@@ -3,19 +3,4 @@ import MasMagicPills
 import XCTest
 
 class DictionaryExtensionsTests: XCTestCase {
-    func test_getorput() {
-        var dictionary = ["a": 1, "b": 2]
-
-        dictionary.keys.forEach { key in
-            let value = dictionary.getOrPut(key) {
-                XCTFail("This never been called because key must exists in the dictionary")
-                return 3
-            }
-            XCTAssertEqual(value, dictionary[key])
-        }
-        XCTAssertEqual(dictionary.keys.count, 2)
-
-        XCTAssertEqual(dictionary.getOrPut("c") { 3 }, 3)
-        XCTAssertEqual(dictionary.keys.count, 3)
-    }
 }
