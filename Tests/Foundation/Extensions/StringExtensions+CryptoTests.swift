c959c7d5a97d59152a9d8f53037b9ee0f8029369
Gabriel-V-Radu <83640956+Gabriel-V-Radu@users.noreply.github.com>
2022-06-13 18:33:48 +0200

    (RAG-4560) Added sha1 for userHash analytics (#18)

--- Tests/Foundation/Extensions/StringExtensions+CryptoTests.swift
+++ Tests/Foundation/Extensions/StringExtensions+CryptoTests.swift
@@ -7,6 +7,10 @@ class StringExtensionsCryptoTests: XCTestCase {
         XCTAssertEqual("hash, hash, baby!!".md5, "A6401501F421EC8CE65C8405915ADF0D")
     }
 
+    func test_sha1() {
+        XCTAssertEqual("📦".sha1, "221887727FDCBD9F648F2672CFD4F366264C8440")
+    }
+
     func test_sha256() {
         XCTAssertEqual("hash, hash, baby!!!! :D".sha256, "5EEBACE75DEDB9C94BE1E72E1229B55A02DBE2D23E5FEE0756CE6CB96673BEEB")
         XCTAssertEqual("📦".sha256, "E68EBC94437CAF84F78CF166B90ABFF24ABF4E95F30897B2A90768B59BFC0A7B")
