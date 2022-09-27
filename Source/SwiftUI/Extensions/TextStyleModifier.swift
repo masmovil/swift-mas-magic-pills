ab1ac69097b5a0a18e3e84ea23e98d56d17c90ba
Edi <edilberto.lopez@masmovil.com>
2022-09-27 15:38:13 +0200

    Add line limit to text style

--- Source/SwiftUI/Extensions/TextStyleModifier.swift
+++ Source/SwiftUI/Extensions/TextStyleModifier.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 public extension Text {
-    func textStyle(_ textStyle: TextStyle) -> some View {
+    func textStyle(_ textStyle: TextStyle, lineLimit: Int? = nil) -> some View {
         self.tracking(textStyle.letterSpacing ?? 0)
             .modifier(TextWithStyle(textStyle))
     }
@@ -9,9 +9,11 @@ public extension Text {
 
 private struct TextWithStyle: ViewModifier {
     private var textStyle: TextStyle
+    private var lineLimit: Int?
 
-    public init(_ textStyle: TextStyle) {
+    public init(_ textStyle: TextStyle, lineLimit: Int? = nil) {
         self.textStyle = textStyle
+        self.lineLimit = lineLimit
     }
 
     public func body(content: Content) -> some View {
@@ -21,7 +23,7 @@ private struct TextWithStyle: ViewModifier {
                 .foregroundColor(textStyle.color)
                 .textCase(textStyle.textCase)
                 .lineSpacing(textStyle.lineSpacing)
-                .lineLimit(nil)
+                .lineLimit(lineLimit)
                 .multilineTextAlignment(textStyle.textAlignment)
         }
     }
