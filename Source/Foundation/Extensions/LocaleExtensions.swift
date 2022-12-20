aeacf55daf3d5c545fb9219b7cf199f2178de2e9
Sebastián Varela <sebastian.varela@masmovil.com>
2022-12-20 16:26:59 +0100

    Fix locales

--- Source/Foundation/Extensions/LocaleExtensions.swift
+++ Source/Foundation/Extensions/LocaleExtensions.swift
@@ -4,21 +4,21 @@ public extension Locale {
     /// Resolved locale that correspond with POSIX standard
     static var posix: Locale { Locale(identifier: "en_US_POSIX") }
 
-    /// Resolved locale that correspond with es_AR
+    /// Resolved locale that correspond with es_AR (🇦🇷)
     static var spanishArgentine: Locale { Locale(identifier: "es_AR") }
 
-    /// Resolved locale that correspond with es_ES
+    /// Resolved locale that correspond with es_ES (🇪🇸)
     static var spanishSpain: Locale { Locale(identifier: "es_ES") }
 
-    /// Resolved locale that correspond with ca_ES
+    /// Resolved locale that correspond with ca_ES (🇪🇸)
     static var catalanSpain: Locale { Locale(identifier: "ca_ES") }
 
-    /// Resolved locale that correspond with eu_ES
+    /// Resolved locale that correspond with eu_ES (🇪🇸)
     static var basqueSpain: Locale { Locale(identifier: "eu_ES") }
 
-    /// Resolved locale that correspond with en_US
+    /// Resolved locale that correspond with en_US (🇺🇸)
     static var englishUSA: Locale { Locale(identifier: "en_US") }
 
-    /// Resolved locale that correspond with en_GB
+    /// Resolved locale that correspond with en_GB (🇬🇧)
     static var englishUnitedKingdom: Locale { Locale(identifier: "en_GB") }
 }
